cb76aa4459664e16035263861aa156b8d04360fe
red2922 <jinthavongsa1@gmail.com>
2023-05-01 18:53:16 -0500

    Added more information

--- Host.py
+++ Host.py
@@ -3,6 +3,8 @@ import os
 import time 
 
 #THIS IS A MALICIOUS PROJECT. ONCE COMPLETED PLEASE DO NOT OPEN ON YOU PERSONAL MACHINE 
+#This is currently for a windows OS but I would like to change it to a Linux. This a majorly a test to see what we can learn from 
+#how a virus like this works. 
 
 
 Host = "0.0.0.0"
@@ -15,3 +17,6 @@ SEPERATOR = "<sep>"
 
 
 s = socket.socket()
+
+s.bind((Host,Port))
+
